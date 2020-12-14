2781fd8c40d205f849869d03f097cc61f2ec4d2b
delev <delyan1@mail.bg>
2020-12-14 11:06:14 +0200

    Change viewModel names for auto map

--- Web/Carpfish.Web.ViewModels/Rigs/RigInListViewModel.cs
+++ Web/Carpfish.Web.ViewModels/Rigs/RigInListViewModel.cs
@@ -1,10 +1,9 @@
 ﻿namespace Carpfish.Web.ViewModels.Rigs
 {
-    using AutoMapper;
     using Carpfish.Data.Models;
     using Carpfish.Services.Mapping;
 
-    public class RigInListViewModel : IMapFrom<Rig>, IHaveCustomMappings
+    public class RigInListViewModel : IMapFrom<Rig>
     {
         public string Name { get; set; }
 
@@ -17,18 +16,5 @@
         public int StepsCount { get; set; }
 
         public int MaterialsCount { get; set; }
-
-        public void CreateMappings(IProfileExpression configuration)
-        {
-            configuration.CreateMap<Rig, RigInListViewModel>()
-                .ForMember(x => x.ImageUrl, opt =>
-                  opt.MapFrom(r => r.Image.Url))
-                .ForMember(x => x.TrophiesCount, opt =>
-                  opt.MapFrom(r => r.Trophies.Count))
-                .ForMember(x => x.StepsCount, opt =>
-                  opt.MapFrom(r => r.Steps.Count))
-                .ForMember(x => x.MaterialsCount, opt =>
-                  opt.MapFrom(r => r.Materials.Count));
-        }
     }
 }
